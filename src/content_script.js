cf2f4604f1a19147ebf14e2657d91a10fc5446d7
Kamran Ahmed <kamranahmed.se@gmail.com>
2017-06-17 01:08:34 +0400

    Fix - Flicker when switching to old style

--- src/content_script.js
+++ src/content_script.js
@@ -1,15 +1,6 @@
-// Clear the timer if it already exists
-window.edgeTimer && window.clearInterval(window.edgeTimer);
-
-function removeEdgeDesign() {
+window.setInterval(function () {
     var edgeDesign = document.getElementsByClassName("edge-design");
     if (edgeDesign[0]) {
         edgeDesign[0].className = edgeDesign[0].className.replace(/\bedge-design\b/, '');
     }
-}
-
-// Dirty dirty timeout fix!
-// TODO: Need to replace it with Mutation observer or chrome's alarm API
-window.edgeTimer = window.setInterval(function () {
-    removeEdgeDesign();
-}, 1000);
+}, 100);
